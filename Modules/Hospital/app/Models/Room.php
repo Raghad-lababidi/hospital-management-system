0173b400806114520596ed7e69e2a7a385a8f71c
Raghad-lababidi <raghad2lababidi@gmail.com>
2024-10-16 19:19:42 +0300

    create room model

--- Modules/Hospital/app/Models/Room.php
+++ Modules/Hospital/app/Models/Room.php
@@ -4,6 +4,10 @@ namespace Modules\Hospital\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Hospital\Enums\RoomSize;
+use Modules\Hospital\Enums\RoomStatus;
+use Modules\Hospital\Enums\RoomType;
+
 // use Modules\Hospital\Database\Factories\RoomFactory;
 
 class Room extends Model
@@ -13,7 +17,22 @@ class Room extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = ['number','type','size','status'];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => RoomStatus::class,
+            'type' => RoomType::class,
+            'size' => RoomSize::class
+        ];
+
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 
     // protected static function newFactory(): RoomFactory
     // {
